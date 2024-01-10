fdc63c6aef42593f2417d903d112138421f299f0
vision800 <netshokotshokovision112@gmail.com>
2024-01-10 09:42:28 +0200

    Add project files.

---
+++ Member.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17.Classes_and_Objects
+{
+    internal class Member
+    {
+        public string name;
+        public int age;
+        public string phone;
+        public string address;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17.Classes_and_Objects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+
+            book.title = "Test";
+            book.author = "Vision";
+            book.pages = 23;
+
+            Console.WriteLine("Book title : " + book.title);
+            Console.WriteLine("Book author : " + book.author);
+            Console.WriteLine("Book pages : " + book.pages);
+
+
+            Member member = new Member();
+            member.name = "Test";
+            member.address = "";
+            member.phone = "1234567890";
+            member.age = 10;
+
+            Console.WriteLine("Member name : " +member.name);
+            Console.WriteLine("Member address: " + member.address);
+            Console.WriteLine("Member phone: " + member.phone);
+            Console.WriteLine("Member age: " + member.age);
+        }
+    }
+}
